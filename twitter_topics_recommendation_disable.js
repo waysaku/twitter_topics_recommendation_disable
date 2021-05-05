b9bf8e9eca3f5b29ce70403682979a30c6660b55
waysaku <waysaku@gmail.com>
2021-05-05 11:57:39 +0900

    update

--- twitter_topics_recommendation_disable.js
+++ twitter_topics_recommendation_disable.js
@@ -1,18 +1,18 @@
-var timerId = window.setInterval((function() {
+var timerId = window.setInterval((function () {
 	var eles = Array.prototype.slice.call(document.getElementsByClassName('r-1p0dtai r-1ei5mc7 r-1pi2tsx r-1d2f490 r-crgep1 r-orgf3d r-t60dpp r-u8s1d r-zchlnj r-ipm5af r-13qz1uu'));
+	var checkedEles = eles.filter((ele) => {
+		return ele.checked;
+	});
 	var count = 0;
-	return function() {
-		if (count > eles.length) {
+
+	return function () {
+		if (count >= checkedEles.length) {
 			window.clearInterval(timerId);
+			console.log('All Done')
 			return;
 		}
-		var ele = eles[count];
-		if (ele.checked === true) {
-			ele.click();
-			console.log(count + ': disabled done');
-			return;
-		}
-		console.log(count + ': nothing to do');
-    count++;
+		checkedEles[count].click();
+		console.log(count + ': Disabled Done');
+		count++;
 	};
 }()), 5000);
